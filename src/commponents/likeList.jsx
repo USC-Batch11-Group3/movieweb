b0822af7206de7eeb3f753496d2be23947bc7597
Aiden <82112194+AidenZ01@users.noreply.github.com>
2021-09-23 10:47:51 -0400

    added pagination and loader
    added pagination and loader

--- src/commponents/likeList.jsx
+++ src/commponents/likeList.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { GlobalContext, GlobalProvider } from "../context/globalState";
+import { GlobalContext } from "../context/globalState";
 
 const LikedMovies = () => {
   const { likeList } = useContext(GlobalContext);
--- src/commponents/movies.jsx
+++ src/commponents/movies.jsx
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from "react";
 import MovieCard from "./movieCard";
 import Container from "./container";
+import Pagination from "./pagination";
+import Loader from "./loader";
 
 const API_KEY = "d9a2a89e1dbe265a06a281f6ad70bbb8";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const fetchMovie = async () => {
+    setLoading(true);
     const movieResponse = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${currentPage}`
     );
     const movieData = await movieResponse.json();
+    setLoading(false);
     setMovies(movieData.results);
   };
 
   useEffect(() => {
     fetchMovie();
-  }, []);
+  }, [currentPage]);
+
+  const newPage = (direction) => {
+    if (direction === "next") {
+      setCurrentPage((prevCurrent) => prevCurrent + 1);
+    } else if (direction === "previous" && currentPage !== 1) {
+      setCurrentPage((prevCurrent) => prevCurrent - 1);
+    }
+  };
 
   return (
     <div>
+      <Pagination data={movies} currentPage={currentPage} newPage={newPage} />
       <Container>
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} data={movie} />
-        ))}
+        {loading ? (
+          <Loader />
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} data={movie} />)
+        )}
       </Container>
     </div>
   );
