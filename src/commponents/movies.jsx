d40985c74ee357fb6035e4d01b3fc4cd90de16f1
ericl-code <ericlin091899@gmail.com>
2021-09-24 09:52:12 -0500

    Sorting
    Title, date, score, count

--- src/commponents/movies.jsx
+++ src/commponents/movies.jsx
@@ -10,6 +10,7 @@ const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortType, setSortType] = useState("asc");
 
   const fetchMovie = async () => {
     setLoading(true);
@@ -33,8 +34,71 @@ const Movies = () => {
     }
   };
 
+  const sortByRated = () => {
+    const sortedList = [...movies].sort((a, b) => {
+      if (sortType === "asc") {
+        setSortType("des");
+        return a.vote_average - b.vote_average;
+      } else {
+        setSortType("asc");
+        return b.vote_average - a.vote_average;
+      }
+    });
+    setMovies(sortedList);
+  };
+
+  const sortByTitle = () => {
+    const sortedList = [...movies].sort((a, b) => {
+      if (sortType === "asc") {
+        setSortType("des");
+        return a.title.localeCompare(b.title);
+      } else {
+        setSortType("asc");
+        return b.title.localeCompare(a.title);
+      }
+    });
+    setMovies(sortedList);
+  };
+
+  const sortByCount = () => {
+    const sortedList = [...movies].sort((a, b) => {
+      if (sortType === "asc") {
+        setSortType("des");
+        return a.vote_count - b.vote_count;
+      } else {
+        setSortType("asc");
+        return b.vote_count - a.vote_count;
+      }
+    });
+    setMovies(sortedList);
+  };
+  const sortByDate = () => {
+    const sortedList = [...movies].sort((a, b) => {
+      if (sortType === "asc") {
+        setSortType("des");
+        return a.release_date > b.release_date ? 1: -1;
+      } else {
+        setSortType("asc");
+        return b.release_date < a.release_date ? -1: 1;
+      }
+    });
+    setMovies(sortedList);
+  };
+
   return (
     <div>
+      <button onClick={sortByTitle}>
+        {sortType === "asc" ? "Title ascending" : "Title descending"}
+      </button>
+      <button onClick={sortByDate}>
+        {sortType === "asc" ? "Date ascending" : "Date descending"}
+      </button>
+      <button onClick={sortByRated}>
+        {sortType === "asc" ? "Score ascending" : "Score descending"}
+      </button>
+      <button onClick={sortByCount}>
+        {sortType === "asc" ? "Count ascending" : "Count descending"}
+      </button>
       <Pagination data={movies} currentPage={currentPage} newPage={newPage} />
       <Container>
         {loading ? (
