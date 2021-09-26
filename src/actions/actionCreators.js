7f8880ee3c49fc87a97b37f04faed82d06a80f60
zuoqingshiye <54818991+zuoqingshiye@users.noreply.github.com>
2021-09-26 14:09:13 -0500

    Delete actionCreators.js

--- src/actions/actionCreators.js
+++
@@ -1,9 +0,0 @@
-import * as actions from "./actionTypes"
-
-//action function example
-export const addLikeMovie = description => ({
-    type: actions.ADD_LIKE,
-        payload: {
-            description:""
-        }
-})
