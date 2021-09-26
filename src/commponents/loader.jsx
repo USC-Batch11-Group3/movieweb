c88e71e7626fb38b34964c88bc38681b0d4de867
zuoqingshiye <tzhao2@kent.edu>
2021-09-26 16:26:41 -0500

    Updates

--- src/commponents/loader.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loader = () => {
-  return <div className="loader">Loading Movies...</div>;
-};
-
-export default Loader;
--- src/commponents/movieStatus.js
+++
@@ -1,9 +0,0 @@
-export function isMovieInLikeList(movie, likeList) {
-  const result = likeList.some((liked) => movie.id === liked.id);
-  return result;
-}
-
-export function isMovieInDiscardList(movie, discardList) {
-  const result = discardList.some((liked) => movie.id === liked.id);
-  return result;
-}
